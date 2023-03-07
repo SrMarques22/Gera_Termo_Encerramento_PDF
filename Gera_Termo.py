48fce24cb6b1f4ba665a0d2f03d1a72bb9aee031
SrMarques22 <ti.brunom@gmail.com>
2023-03-07 08:50:24 -0300

    Projeto de geração de Termo de encerramento Linx em PDF

---
+++ Gera_Termo.py
@@ -0,0 +1,6 @@
+import pdfkit
+
+#O Arquivo abaixo tem que ser baixado em: https://wkhtmltopdf.org/downloads.html depois, deve-se apontar para o caminho onde exe está!
+config = pdfkit.configuration(wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
+
+
---
+++ Readme.md
@@ -0,0 +1,4 @@
+Para mais formas de geração de PDF como por exemplo a partir de uma página HTML,
+podemos utilizar o link abaixo:
+
+https://jobu.com.br/2021/06/23/como-criar-arquivos-pdf-com-python/
